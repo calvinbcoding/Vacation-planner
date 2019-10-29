3e5f78da232195ffe303486946beac5e2b36d995
Calvin Bruch <calvinbcoding@gmail.com>
2019-10-29 15:12:41 -0600

    added templating

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -1,7 +1,22 @@
+
+
+
+
+
+
+
+
 class ApplicationController < Sinatra::Base
     require 'bundler'
     Bundler.require()
 
+    
+    set :views, File.expand_path('../../views', __FILE__)
+
+    #teach it how to view static assets
+    set :public_dir, File.expand_path('../../public', __FILE__)
+
+    
     get '/' do
         "This is your Vacation Planner."
     end
@@ -12,4 +27,9 @@ class ApplicationController < Sinatra::Base
         
         "pry has finished -- here's sample_message #{sample_message}"
     end
+
+    get '/splashpage' do
+        erb :splashpage
+    end
+
 end
